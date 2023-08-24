311d556097174d626af5c8b38ec5ef7fce91b8e3
Koda777 <alexandre.decobert@epitech.eu>
2023-08-24 18:17:32 +0200

    [ADD] color lineNr

--- init.lua
+++ init.lua
@@ -1,10 +1,14 @@
-local sources = {
-	"settings",
-}
+local impatient_ok, impatient = pcall(require, "impatient")
 
-for _, source in ipairs(sources) do
+if impatient_ok then
+	impatient.enable_profile()
+end
+
+for _, source in ipairs({
+	"sensational.lua.settings",
+}) do
 	local status_ok, fault = pcall(require, source)
-	if status_ok then
-		source.config()
+	if not status_ok then
+		error("Error loading " .. source .. "\n\n" .. fault)
 	end
 end
--- lua/settings.lua
+++ lua/settings.lua
@@ -1,15 +1,43 @@
-local M = {}
+local cmd = vim.cmd
+local g = vim.g
+local fn = vim.fn
 
-function M.config()
-	if not vim.fn.has("gui_running") and vim.fn.t_Co < 256 then
-		return
-	end
-	-- Clear highlighting
-	vim.cmd("hi clear")
-	if vim.fn.exists("syntax_on") then
-		vim.cmd("syntax reset")
-	end
-	vim.g.colors_name = "sensational"
+if not fn.has("gui_running") and vim.fn.t_Co < 256 then
+	return
 end
 
-return M
+-- Clear highlighting
+cmd("hi clear")
+if fn.exists("syntax_on") then
+	cmd("syntax reset")
+end
+g.colors_name = "sensational"
+
+local highlight = {
+	-- Background color
+	"Normal guibg=#1D1E20",
+	"Identifier cterm=bold ctermfg=14 guifg=#8CB4B4",
+	-- Function name
+	"Function cterm=bold ctermfg=14 guifg=#FFE3DC",
+	-- Delimiter ({}, [], (), <>})
+	"Delimiter cterm=bold ctermfg=14 guifg=White",
+	-- Operator, name function ect
+	"Comment cterm=bold ctermfg=14 guifg=#8CB4B4",
+	-- color function type
+	"Statement cterm=bold ctermfg=14 guifg=#5B677F",
+	-- Window open
+	"Pmenu guibg=#1D1E20",
+	-- div tab ect
+	"Special guifg=#8CB4B4",
+	-- import, from module ect
+	"PreProc guifg=#DEC07F",
+	-- Logo folder color
+	"Title guifg=Yellow",
+	"ColorColumn guifg=Red",
+	"LineNr guifg=#8CB4B4",
+	"CursorLineNr guifg=#669B9B",
+}
+
+for _, v in ipairs(highlight) do
+	cmd("hi! " .. v)
+end
